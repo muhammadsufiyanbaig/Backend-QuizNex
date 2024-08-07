4e113271ab996794d914e57ba353fe7730fec93a
muhammadsufiyanbaig <send.sufiyan@gmail.com>
2024-08-07 17:53:49 +0500

    update user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,11 @@ const { signup, login, logout, getUser, updateUser, deleteUser } = require("../c
 const authenticate = require("../middleware/authMiddleware");
 const router = express.Router();
 
-router.post("/auth/signup", signup);
-router.post("/auth/login", login);
-router.get("/auth/logout", logout);
+router.post("/signup", signup);
+router.post("/login", login);
+router.get("/logout", logout);
 router.post("/portal", authenticate, getUser);
-router.put("/auth/update", authenticate, updateUser);
-router.delete("/auth/delete", authenticate, deleteUser);
+router.put("/update", authenticate, updateUser);
+router.delete("/delete", authenticate, deleteUser);
 
 module.exports = router;
